8dc6d656e409c6d0db342edcf15e6288aac3516f
BlivionIaG <kev29lt@gmail.com>
2019-03-05 11:08:31 +0100

    sav

--- controle_1/fonc_div.h
+++ controle_1/fonc_div.h
@@ -2,6 +2,18 @@
 //prototype fonctions 
 //control_1
 
+void init_port_SPI(void);
+
+void affiche_mot(uint8_t *mot, uint8_t col, uint8_t ligne);
+
+void affiche_temp(uint16_t nombre, uint8_t ligne, uint8_t col);
+
+void affiche_puis(uint8_t nombre, uint8_t ligne, uint8_t col);
+
+void affiche_etat_fen(unsigned char fermee);
+
+void init_timer2_pwm(void);
+
 
 
 
